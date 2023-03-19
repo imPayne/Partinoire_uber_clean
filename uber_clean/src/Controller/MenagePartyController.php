e7c243e40752d95e6a081771a931f95c88fa55bc
imPayne <hugo.monchablon@epitech.eu>
2023-03-19 18:36:40 +0100

    edited footer and menageParty page

--- uber_clean/src/Controller/MenagePartyController.php
+++ uber_clean/src/Controller/MenagePartyController.php
@@ -5,13 +5,15 @@ namespace App\Controller;
 use App\Repository\HouseworkRepository;
 use App\Repository\ServiceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class MenagePartyController extends AbstractController
 {
     #[Route('/menage/party', name: 'app_menage_party')]
-    public function index(HouseworkRepository $houseworkRepository): Response
+    public function index(FormFactoryInterface $formFactory, HouseworkRepository $houseworkRepository, Request $request): Response
     {
         $houseworks = $houseworkRepository->findAll();
 
