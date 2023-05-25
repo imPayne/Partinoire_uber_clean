6d98450bd8028ab450ca1d1a9a3cd130a362cef3
imPayne <hugo.monchablon@epitech.eu>
2023-05-25 04:08:00 +0200

    added updates in user Fixtures, edit customer data and added new user Administrator

--- stud_clean/src/DataFixtures/UserFixtures.php
+++ stud_clean/src/DataFixtures/UserFixtures.php
@@ -23,17 +23,27 @@ class UserFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $user = new Customer();
-        $user->setFirstName('admin');
-        $user->setEmail("admin@gmail.com");
-        $user->setAdresse("2 rue de l'admin");
-        $user->setCodePostal("35000");
+        $user->setFirstName('hugo');
+        $user->setEmail("hugo@gmail.com");
+        $user->setAdresse("2 rue de la cacahouète");
+        $user->setCodePostal("35200");
         $user->setRegion("Bretagne");
-        $user->setLastName("Administrator");
+        $user->setLastName("Développeur");
         $user->setRoles(['ROLE_CUSTOMER']);
         $user->setImage("gabimaru-anime-646aa6dcccd16.gif");
         $user->setPhoneNumber("0772378558");
-        $password = $this->hasher->hashPassword($user, 'admin');
+        $password = $this->hasher->hashPassword($user, '5445HUhu00');
         $user->setPassword($password);
+
+        $admin = new User();
+        $admin->setFirstName("admin");
+        $admin->setLastName("administrateur");
+        $admin->setImage("5df0fa20efbbfc61d1c419fb42b9c4670f1610cdr1-540-304-hq-6463dfe6ed33b.gif");
+        $admin->setRoles("ROLE_ADMIN");
+        $admin->setPassword($this->hasher->hashPassword($admin, "admin"));
+        $admin->setEmail("admin@gmail.com");
+        $manager->persist($admin);
+
 
         $newHousework = new Housework();
         $newHousework->setPrice(40);
