eeaa75318a5b7ec81eca686156fe6754ffc4c729
imPayne <hugo.monchablon@epitech.eu>
2023-05-25 04:13:30 +0200

    fixed error in fixtures usage

--- stud_clean/src/DataFixtures/UserFixtures.php
+++ stud_clean/src/DataFixtures/UserFixtures.php
@@ -35,13 +35,17 @@ class UserFixtures extends Fixture
         $password = $this->hasher->hashPassword($user, '5445HUhu00');
         $user->setPassword($password);
 
-        $admin = new User();
+        $admin = new Customer();
         $admin->setFirstName("admin");
         $admin->setLastName("administrateur");
         $admin->setImage("5df0fa20efbbfc61d1c419fb42b9c4670f1610cdr1-540-304-hq-6463dfe6ed33b.gif");
-        $admin->setRoles("ROLE_ADMIN");
+        $admin->setRoles(["ROLE_ADMIN"]);
         $admin->setPassword($this->hasher->hashPassword($admin, "admin"));
         $admin->setEmail("admin@gmail.com");
+        $admin->setAdresse("6 avenue de l'admin");
+        $admin->setCodePostal("35000");
+        $admin->setPhoneNumber("0772378592");
+        $admin->setRegion("Bretagne");
         $manager->persist($admin);
 
 
